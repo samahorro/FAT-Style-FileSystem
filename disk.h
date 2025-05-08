848a99412cbc202267b5f4aed15d8d90634c8a79
samahorro <sahorro8@csu.fullerton.edu>
2025-05-08 13:39:28 -0700

    Added Functions to Header

--- disk.h
+++ disk.h
@@ -1,5 +1,10 @@
-int create_disk(char *name);
+#ifndef DISK_H
+#define DISK_H
+
+int make_disk(char *name); // Or create_disk(), pick one
 int open_disk(char *name);
 int close_disk();
 int block_write(int block, char *buf);
 int block_read(int block, char *buf);
+
+#endif
